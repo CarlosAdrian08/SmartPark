0bb62afee58e464e07a63a02c420f7695307d10a
RubenCN <rubencanulnohgit@gmail.com>
2026-03-23 15:40:59 -0600

    feat(constants): Importamos los colores a utilizar en la app

---
+++ constants/Colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  primary: "#007F80",
+  background: "#F5F7FA",
+  surface: "#FFFFFF",
+  textPrimary: "#0F172A",
+  textSecondary: "#0D9488",
+  textMuted: "#6B7280",
+  danger: "#EF4444",
+  navy: "#1E3A8A",
+};
