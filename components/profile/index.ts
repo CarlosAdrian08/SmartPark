4f209812c7097bdf512230d266413034a48e2981
Santiago <l22070087@valladolid.tecnm.mx>
2026-05-15 17:43:33 -0600

    feat(profile): agregar componentes de perfil para exportación

---
+++ components/profile/index.ts
@@ -0,0 +1,5 @@
+export { default as ProfileHeader } from "./ProfileHeader";
+export { default as ProfileItem } from "./ProfileItem";
+export { default as ProfileModal } from "./ProfileModal";
+export { default as ProfileSection } from "./ProfileSection";
+
